48d6554bf01cff5ce54b008fabbae13fc1bfd4f4
fallingofman <thefallingofman@gmail.com>
2025-01-20 17:51:37 -0500

    proof of concept script

--- InstagramMain.py
+++ InstagramMain.py
@@ -1,6 +1,6 @@
 from instagrapi import Client
-
-
+from urllib.request import urlretrieve
+import os
 
 def login(client):
 	client.login("hairy_choiga_23", "787newpassword")
@@ -11,5 +11,19 @@ if __name__ == "__main__":
 
 	user_id = cl.user_id_from_username("oppyolly")
 	medias = cl.user_medias(user_id, 20)
-	print(medias)
+	urls = [media.video_url for media in medias]
+	print(urls)
+	print(len(urls))
+
+	for i in range(0, len(urls)):
+		print
+		url = urls[i]
+		if url is not None:
+			urlretrieve(str(url), "oppyolly_videos_" + str(i) + ".mp4")
+
+	cl.clip_upload(os.path.dirname(__file__) + "/oppyolly_videos_10.mp4", "hello this is a test from instagrapi")
+
+
+
+
 
