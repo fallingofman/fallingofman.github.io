4ab00ed716374ff082f027d24e193d43c464d532
oxue <oxu.wex@gmail.com>
2025-01-24 00:28:58 -0500

    use correct variables and indent using spaces, previously we were using the globals and not the argument "client"

--- InstagramMain.py
+++ InstagramMain.py
@@ -4,53 +4,53 @@ import os
 import argparse
 
 meme_page_usernames = ["oppyolly"]
-number_of_daily_posts = 10
+number_of_daily_posts = 1
 
 def login(client):
-	client.login("hairy_choiga_23", "787newpassword")
+    client.login("hairy_choiga_23", "787newpassword")
 
 def pull_meme_data(client, meme_page_usernames):
-	user_media_map = {}
-	for username in meme_page_usernames:
-		user_id = cl.user_id_from_username(username)
-		user_media_map[username] = cl.user_medias(user_id, 20)
-	print(user_media_map)
-	return user_media_map
+    user_media_map = {}
+    for username in meme_page_usernames:
+        user_id = client.user_id_from_username(username)
+        user_media_map[username] = client.user_medias(user_id, 20)
+    print(user_media_map)
+    return user_media_map
 
 def download_videos_for_upload(user_media_map):
-	all_media = user_media_map.values()
-	all_media = sum(all_media, [])
-	all_media = sorted(all_media, key=lambda post: post.like_count)
-	all_media = all_media[:number_of_daily_posts]
-	print(len(all_media))
-	for i in range(0, number_of_daily_posts):
-		url = all_media[i].video_url 
-		if url is not None:
-			urlretrieve(str(url), "daily_videos_" + str(i) + ".mp4")
+    all_media = user_media_map.values()
+    all_media = sum(all_media, [])
+    all_media = sorted(all_media, key=lambda post: post.like_count)
+    all_media = all_media[:number_of_daily_posts]
+    print(len(all_media))
+    for i in range(0, number_of_daily_posts):
+        url = all_media[i].video_url 
+        if url is not None:
+            urlretrieve(str(url), "daily_videos_" + str(i) + ".mp4")
 
 def upload_videos(client):
-	files = os.listdir('.')
-	files = [file for file in files if "daily_videos" in file]
-	if len(files) > 0:
-		next_file_upload_name = files[0]
-		cl.clip_upload(os.path.dirname(__file__) + "/" + next_file_upload_name, "hello this is a test from instagrapi")
-		os.remove(next_file_upload_name)
+    files = os.listdir('.')
+    files = [file for file in files if "daily_videos" in file]
+    if len(files) > 0:
+        next_file_upload_name = files[0]
+        client.clip_upload(os.path.dirname(__file__) + "/" + next_file_upload_name, "hello this is a test from instagrapi")
+        os.remove(next_file_upload_name)
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--state', help='1: download, 2: upload')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--state', help='1: download, 2: upload')
+    args = parser.parse_args()
 
-	cl = Client()
-	login(cl)
+    cl = Client()
+    login(cl)
 
-	if args.state == "1":
-		print(args.state)
-		user_media_map = pull_meme_data(cl, meme_page_usernames)
-		print(user_media_map)
-		download_videos_for_upload(user_media_map)
-	if args.state == "2":
-		upload_videos(cl)
+    if args.state == "1":
+        print(args.state)
+        user_media_map = pull_meme_data(cl, meme_page_usernames)
+        print(user_media_map)
+        download_videos_for_upload(user_media_map)
+    if args.state == "2":
+        upload_videos(cl)
 
 
 
